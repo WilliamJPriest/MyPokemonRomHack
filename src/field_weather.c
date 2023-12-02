6dd02755fdf65b17f83afc28b54e8234113a7c73
williamjpriest <liuliu6wp@gmail.com>
2023-12-02 09:38:30 -0500

    dif error

--- src/field_weather.c
+++ src/field_weather.c
@@ -100,7 +100,7 @@ static const struct WeatherCallbacks sWeatherFuncs[] =
     [WEATHER_DROUGHT]            = {Drought_InitVars,       Drought_Main,       Drought_InitAll,       Drought_Finish},
     [WEATHER_DOWNPOUR]           = {Downpour_InitVars,      Thunderstorm_Main,  Downpour_InitAll,      Thunderstorm_Finish},
     [WEATHER_UNDERWATER_BUBBLES] = {Bubbles_InitVars,       Bubbles_Main,       Bubbles_InitAll,       Bubbles_Finish},
-    [WEATHER_SHADE2]              = {Shade_InitVars,         Shade_Main,         Shade_InitAll,         Shade_Finish},
+    [WEATHER_SHADE2]             = {Shade_InitVars,         Shade_Main,         Shade_InitAll,         Shade_Finish},
 };
 
 void (*const gWeatherPalStateFuncs[])(void) =
@@ -384,7 +384,7 @@ static void FadeInScreenWithWeather(void)
     case WEATHER_RAIN_THUNDERSTORM:
     case WEATHER_DOWNPOUR:
     case WEATHER_SHADE:
-    case WEATHER_SHADE2:
+    
         if (FadeInScreen_RainShowShade() == FALSE)
         {
             gWeatherPtr->colorMapIndex = 3;
@@ -778,7 +778,6 @@ void FadeScreen(u8 mode, s8 delay)
     case WEATHER_DOWNPOUR:
     case WEATHER_FOG_HORIZONTAL:
     case WEATHER_SHADE:
-    case WEATHER_SHADE2:
     case WEATHER_DROUGHT:
         useWeatherPal = TRUE;
         break;
