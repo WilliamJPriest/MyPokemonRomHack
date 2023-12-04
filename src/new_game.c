016a0d9fd3bb390a2de30400d66e944854783f4d
williamjpriest <liuliu6wp@gmail.com>
2023-12-04 15:58:08 -0500

    new local

--- src/new_game.c
+++ src/new_game.c
@@ -126,7 +126,7 @@ static void ClearFrontierRecord(void)
 
 static void WarpToTruck(void)
 {
-    SetWarpDestination(MAP_GROUP(INSIDE_OF_TRUCK), MAP_NUM(INSIDE_OF_TRUCK), WARP_ID_NONE, -1, -1);
+    SetWarpDestination(MAP_GROUP(CENTER_CITY), MAP_NUM(CENTER_CITY), -1, -1, -1);
     WarpIntoMap();
 }
 
