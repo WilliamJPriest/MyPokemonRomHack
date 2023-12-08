66786a72a3a1fe25960940143929260b01f2897e
williamjpriest <liuliu6wp@gmail.com>
2023-12-08 13:22:33 -0500

    change start local

--- src/new_game.c
+++ src/new_game.c
@@ -126,7 +126,7 @@ static void ClearFrontierRecord(void)
 
 static void WarpToTruck(void)
 {
-    SetWarpDestination(MAP_GROUP(PROF_LAB), MAP_NUM(PROF_LAB), -1, -1, -1);
+    SetWarpDestination(MAP_GROUP(NEW-MAP1), MAP_NUM(NEW-MAP1), -1, -1, -1);
     WarpIntoMap();
 }
 
